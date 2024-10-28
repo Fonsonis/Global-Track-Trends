9162f8204b7e83ff47972014f0db61a0e9cc0c26
Fonsonis <daniel290188@gmail.com>
2024-10-28 22:46:03 +0100

    Changing port because of mac services

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
